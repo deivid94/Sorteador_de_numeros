08e495f2de71f2a0e34b262b600d96b5da3ebea3
deivid94 <md031194@gmail.com>
2025-07-01 01:16:37 -0300

    js: create function for sort numbers

---
+++ index.js
@@ -0,0 +1,12 @@
+const quantidadeNumerosDeSorteio = document.querySelectorAll("")
+const InicioIntervaloDeNumerosSorteio = document.querySelectorAll("")
+const  FimIntervaloDeNumerosSorteio= document.querySelectorAll("")
+
+
+function RealizarSorteio (quantidadeNumerosDeSorteio, InicioIntervaloDeNumerosSorteio, FimIntervaloDeNumerosSorteio){
+
+    while (!quantidadeNumerosDeSorteio){
+
+    }
+    
+}
