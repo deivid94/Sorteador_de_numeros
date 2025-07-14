5722915fedc1fa0e94fe208130414a2fd30dcbd4
deivid94 <md031194@gmail.com>
2025-07-14 00:39:19 -0300

    JS: implementando a funcao de sortear numeros

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-const quantidadeNumerosDeSorteio = document.querySelectorAll("")
-const InicioIntervaloDeNumerosSorteio = document.querySelectorAll("")
-const  FimIntervaloDeNumerosSorteio= document.querySelectorAll("")
+const quantidadeNumerosDeSorteio = document.getElementById("numero1");
+quantidadeNumerosDeSorteio.addEventListener("input", function (e) {
+  let quanitdadeNumeroSorteio = e.target.value;
+  e.target.value = quanitdadeNumeroSorteio.replace(/[^\d]/g, "");
+});
 
+const InicioIntervaloDeNumerosSorteio = document.getElementById("numero2");
+InicioIntervaloDeNumerosSorteio.addEventListener("input", (e) => {
+  let inicialNumeroSorteio = e.target.value;
+  e.target.value = inicialNumeroSorteio.replace(/[^\d]/g, "");
+});
 
-function RealizarSorteio (quantidadeNumerosDeSorteio, InicioIntervaloDeNumerosSorteio, FimIntervaloDeNumerosSorteio){
+const FimIntervaloDeNumerosSorteio = document.getElementById("numero3");
+FimIntervaloDeNumerosSorteio.addEventListener("input", (e) => {
+  let terminalNumeroSorteio = e.target.value;
+  e.target.value = terminalNumeroSorteio.replace(/[^\d]/g, "");
+});
 
-    while (!quantidadeNumerosDeSorteio){
+const formulario = document.getElementById(formularioSorteio);
+formulario.addEventListener("submit", RealizarSorteio);
 
-    }
-    
+function RealizarSorteio(quantidadeVezes, NumeroInicial, NumeroFinal) {
+  event.preventDefault();
+  if (NumeroInicial > NumeroFinal) {
+    alert("Numero inicial menor queo o numero final");
+    return;
+  }
+  const resultado = [];
+
+  for (let i = 0; i < quantidadeVezes; i++) {
+    const numeroSorteado = Math.floor(
+      Math.random() * (NumeroFinal - NumeroInicial + 1) + NumeroInicial
+    );
+
+    resultado.push(numeroSorteado);
+    alert(resultado);
+  }
+  return resultado;
 }
+
+console.log(
+  RealizarSorteio(
+    quantidadeNumerosDeSorteio,
+    InicioIntervaloDeNumerosSorteio,
+    FimIntervaloDeNumerosSorteio
+  )
+);
