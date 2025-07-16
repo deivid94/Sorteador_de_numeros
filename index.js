1fb3840bf2a942c1760d4c9c71aab3058bc71e04
deivid94 <md031194@gmail.com>
2025-07-16 00:53:52 -0300

    style: add modal for sort numbers

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const quantidadeNumerosDeSorteio = document.getElementById("numero1");
-quantidadeNumerosDeSorteio.addEventListener("input", function (e) {
-  let quanitdadeNumeroSorteio = e.target.value;
-  e.target.value = quanitdadeNumeroSorteio.replace(/[^\d]/g, "");
-});
-
-const InicioIntervaloDeNumerosSorteio = document.getElementById("numero2");
-InicioIntervaloDeNumerosSorteio.addEventListener("input", (e) => {
-  let inicialNumeroSorteio = e.target.value;
-  e.target.value = inicialNumeroSorteio.replace(/[^\d]/g, "");
-});
-
-const FimIntervaloDeNumerosSorteio = document.getElementById("numero3");
-FimIntervaloDeNumerosSorteio.addEventListener("input", (e) => {
-  let terminalNumeroSorteio = e.target.value;
-  e.target.value = terminalNumeroSorteio.replace(/[^\d]/g, "");
-});
-
-const formulario = document.getElementById(formularioSorteio);
-formulario.addEventListener("submit", RealizarSorteio);
-
 function RealizarSorteio(quantidadeVezes, NumeroInicial, NumeroFinal) {
   event.preventDefault();
   if (NumeroInicial > NumeroFinal) {
@@ -38,10 +17,29 @@ function RealizarSorteio(quantidadeVezes, NumeroInicial, NumeroFinal) {
   return resultado;
 }
 
-console.log(
-  RealizarSorteio(
-    quantidadeNumerosDeSorteio,
-    InicioIntervaloDeNumerosSorteio,
-    FimIntervaloDeNumerosSorteio
-  )
-);
+const quantidadeNumerosDeSorteio = document.getElementById("numero1");
+quantidadeNumerosDeSorteio.addEventListener("input", function (e) {
+  let valor = e.target.value;
+  e.target.value = valor.replace(/[^\d]/g, "");
+});
+
+const InicioIntervaloDeNumerosSorteio = document.getElementById("numero2");
+InicioIntervaloDeNumerosSorteio.addEventListener("input", (e) => {
+  let valor = e.target.value;
+  e.target.value = valor.replace(/[^\d]/g, "");
+});
+
+const FimIntervaloDeNumerosSorteio = document.getElementById("numero3");
+FimIntervaloDeNumerosSorteio.addEventListener("input", (e) => {
+  let valor = e.target.value;
+  e.target.value = valor.replace(/[^\d]/g, "");
+});
+
+const botaoSortear = document.getElementById("realizar-sorteio");
+botaoSortear.addEventListener("click", function () {
+  const quantidade = quantidadeNumerosDeSorteio.value;
+  const inicio = InicioIntervaloDeNumerosSorteio.value;
+  const fim = FimIntervaloDeNumerosSorteio.value;
+
+  RealizarSorteio(quantidade, inicio, fim);
+});
